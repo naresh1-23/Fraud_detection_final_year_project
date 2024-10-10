316465543bd8da7f515eddc8d985f7aa8faa0c13
naresh1-23 <nareshtamang20000@gmail.com>
2024-10-10 19:53:07 +0545

    feat: added functionality of login and register

--- app/views/user_views.py
+++ app/views/user_views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.services.user_service import UserService
 
 
 def user_login(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get("password")
+        status, message, obj = UserService().login_user(email, password)
+        print(message)
+        if status:
+            return redirect('home')
+        return redirect('login')
     return render(request, 'authentication/login.html')
 
 
 def register(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        role = request.POST.get('role')
+        status, message, obj = UserService().register_user(email, password, confirm_password, role)
+        print(message)
+        if status:
+            return redirect('login')
+        return redirect('register')
     return render(request, 'authentication/register.html')
