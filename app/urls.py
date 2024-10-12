ba6039b5a51a3e59dccb3269a542bd6dd3c1d514
naresh1-23 <nareshtamang20000@gmail.com>
2024-10-12 18:15:35 +0545

    feat: dynamic product rendering

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('home/', product_views.home, name='home'),
     path('add_product/', seller_views.add_product, name='add product'),
-    path('bid/', product_views.bid, name='bid'),
+    path('bid/<int:pk>/', product_views.bid, name='bid'),
     path('data_listing/', prakash_views.data_listing, name='data listing'),
     path('win/', prakash_views.win, name='won')
 ]
