d595ad3268cb9a72195d0bd61769bcd826af4075
prakash882 <pt422194@gmail.com>
2024-10-18 12:18:16 +0545

    edit profile and bidder edit profile added

--- app/views/prakash_views.py
+++ app/views/prakash_views.py
@@ -5,3 +5,9 @@ def data_listing(request):
 
 def win(request):
     return render(request,"seller/win.html")
+
+def edit_profile(request):
+    return render(request,"authentication/edit_profile.html")
+
+def bidder_edit_profile(request):
+    return render(request,"authentication/bidder_edit_profile.html")
