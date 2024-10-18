f8ef2622fedcb5deb1d6ab12587e3c07308af35b
prakash882 <pt422194@gmail.com>
2024-10-19 01:24:56 +0545

    added your product, bidder bids and bid won

--- app/views/prakash_views.py
+++ app/views/prakash_views.py
@@ -11,3 +11,12 @@ def edit_profile(request):
 
 def bidder_edit_profile(request):
     return render(request,"authentication/bidder_edit_profile.html")
+
+def your_product(request):
+    return render(request,"product/your_product.html")
+
+def bidder_bids(request):
+    return render(request,"product/bidder_bids.html")
+
+def bids_won(request):
+    return render(request,"product/bids_won.html")
