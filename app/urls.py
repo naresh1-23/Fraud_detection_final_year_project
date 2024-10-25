f6d778700fa66c531524f0285d88304ccc0511a2
naresh1-23 <nareshtamang20000@gmail.com>
2024-10-25 19:38:30 +0545

    feat: added profile edit

--- app/urls.py
+++ app/urls.py
@@ -8,14 +8,13 @@ from app.views import prakash_views
 urlpatterns = [
     path('login/', user_views.user_login, name='login'),
     path('register/', user_views.register, name='register'),
-    path('home/', product_views.home, name='home'),
+    path('', product_views.home, name='home'),
     path('add_product/', seller_views.add_product, name='add product'),
     path('bid/<int:pk>/', product_views.bid, name='bid'),
     path('logout/', user_views.user_logout, name='logout'),
     path('data_listing/', dataset_views.data_listing, name='data listing'),
     path('win/', prakash_views.win, name='won'),
-    path('edit_profile/', prakash_views.edit_profile, name='edit profile'),
-    path('bidder_edit_profile/', prakash_views.bidder_edit_profile, name='bidder edit profile'),
+    path('edit_profile/', user_views.profile, name='profile'),
     path('your_product/', prakash_views.your_product, name='your product'),
     path('bidder_bids/', prakash_views.bidder_bids, name='bidder bids'),
     path('bids_won/', prakash_views.bids_won, name='bids won')
--- app/views/prakash_views.py
+++ app/views/prakash_views.py
@@ -5,14 +5,6 @@ def win(request):
     return render(request, "seller/win.html")
 
 
-def edit_profile(request):
-    return render(request, "authentication/edit_profile.html")
-
-
-def bidder_edit_profile(request):
-    return render(request, "authentication/bidder_edit_profile.html")
-
-
 def your_product(request):
     return render(request, "product/your_product.html")
 
