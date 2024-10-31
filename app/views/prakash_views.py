a008b9d77df9e23c74e9ac5a4bab81ef3320b5b7
naresh1-23 <nareshtamang20000@gmail.com>
2024-10-31 12:25:10 +0545

    feat: fixed some issue and added search feature

--- app/views/prakash_views.py
+++ app/views/prakash_views.py
@@ -1,13 +1,1 @@
 from django.shortcuts import render
-
-
-def win(request):
-    return render(request, "seller/win.html")
-
-
-def bidder_bids(request):
-    return render(request, "product/bidder_bids.html")
-
-
-def bids_won(request):
-    return render(request, "product/bids_won.html")
--- app/views/product_views.py
+++ app/views/product_views.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from app.models import Bidding
+from app.models import Bidding, BiddingWinner
 from app.services.product_service import ProductService
 
 
 def home(request):
-    products = ProductService().filter_product(bidding_ending_date__gte=datetime.now())
+    if request.method == "POST":
+        search = request.POST.get('search', "")
+        products = ProductService().filter_product(bidding_ending_date__gte=datetime.now(), name__icontains=search)
+    else:
+        products = ProductService().filter_product(bidding_ending_date__gte=datetime.now())
     data = {
         "products": products
     }
+
     return render(request, 'product/home.html', data)
 
 
@@ -30,3 +35,42 @@ def bid(request, pk):
         messages.success(request, "Successfully bid added")
         return render(request, 'product/bid.html', data)
     return render(request, 'product/bid.html', data)
+
+
+def bidder_bids(request):
+    user = request.user
+    bids = Bidding.objects.filter(bidder=user)
+    total_bids = []
+    if bids:
+        for bid in bids:
+            temp = {}
+            if bid.product.bidding_ending_date > datetime.now().date():
+                status = "Pending"
+            elif bid.product.bidding_ending_date == datetime.now().date():
+                if bid.product.bidding_ending_time < datetime.now().time():
+                    result = BiddingWinner.objects.filter(bidding=bid).first()
+                    if result:
+                        status = "Won"
+                    else:
+                        status = "Loss"
+                else:
+                    status = "Pending"
+            else:
+                result = BiddingWinner.objects.filter(bidding=bid).first()
+                if result:
+                    status = "Won"
+                else:
+                    status = "Loss"
+
+            temp["bid"] = bid
+            temp["status"] = status
+            total_bids.append(temp)
+    print(bids)
+    print(total_bids[0]["bid"])
+    return render(request, "product/bidder_bids.html", {"bids": total_bids})
+
+
+def bids_won(request):
+    user = request.user
+    bids = BiddingWinner.objects.filter(bidding__bidder=user)
+    return render(request, "product/bids_won.html", {"bids": bids})
